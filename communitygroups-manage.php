aeb11452fe546f1782f97577d75e97942acb4b13
Joseph Holland <joseph_holland@outlook.com>
2021-07-29 10:02:15 +0100

    Rename Plugin

---
+++ communitygroups-manage.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name: Community Groups Manager 
+ * Plugin URI: github.com/jhollandaise/communitygroups-manager
+ * Description: Handles ANC Community Groups data  
+ * Version: 0.0.1
+ * Author: Joseph Holland
+ * Author URI: jhol.land
+ * Licence: GPLv2 
+ * Licence URI: https://www.gnu.org/licenses/old-licenses/gpl-2.0.html 
+ */
+
+// 
