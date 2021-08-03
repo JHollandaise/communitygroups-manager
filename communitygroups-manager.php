390beb1435966452ae970c3e8541d41c45fa9c2f
Joseph Holland <joseph_holland@outlook.com>
2021-08-03 14:41:00 +0000

    Add Missing Parenthesis

--- communitygroups-manager.php
+++ communitygroups-manager.php
@@ -50,7 +50,7 @@ function cg_update_posts() {
     $relevant_groups_to_tags = array_filter($groups_to_tags, function($x) use ($relevant_tag_id)
                             {return in_array($relevant_tag_id, $x);});
     // Seeing group[id]==0 for some reason, remove it here
-    unset($relevant_groups_to_tags["0"];
+    unset($relevant_groups_to_tags["0"]);
 
     $relevant_cs_group_ids = array_keys($relevant_groups_to_tags);
     $relevant_wp_posts_ids = get_posts([
